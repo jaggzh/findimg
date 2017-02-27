78dfe53b536981345bf21af7569579aa482b2a82
Jaggzh <jaggz.h@gmail.com>
2017-02-26 22:16:45 -0800

    This version works. Should work for you too.

--- findimg.py
+++ findimg.py
@@ -38,8 +38,10 @@ from os import listdir
 from os.path import isdir, isfile, join
 from PIL import Image
 import PIL
+import matplotlib.pyplot as plt
 
-seed = 7; numpy.random.seed(seed)
+seed = 7; numpy.random.seed(seed); random.seed(seed)
+labeled_frac = .8 # Fraction of generated images coming from labeled set(s)
 indim = 64
 args = None
 allglobs = None
@@ -48,7 +50,6 @@ lbld = None
 test_frac = .10
 val_frac = .10
 model = None
-labeled_frac = .5 # Fraction of generated images coming from labeled set(s)
 label_names = []
 label_ints = dict()
 label_onehots = dict()
@@ -58,13 +59,20 @@ lbl_cnt = None
 load_weights = True
 weight_store = "dat/weights.h5"
 save_weights = True
-train_epochs = 100
+train_epochs = 300
 train_samples = 250
 save_weight_secs = 20
 start_time = time.time()
 last_epoch_time = None
 fit_start_time = None
 checkpoint_epoch = None
+
+# For view_img()
+axs = None
+whichsubplot = -1
+fig_view = None
+fig_view_num = 1
+fig_filt_num = 2
 
 def load_globs():
 	global globsets
@@ -79,16 +87,20 @@ def load_globs():
 			filtered.append(fn)
 	#for x in globs: print(x)
 	globs = filtered
-	random.shuffle(globs)
+	if not args.noshuffle:
+		random.shuffle(globs)
 	allglobs = globs
+	#allglobs = allglobs[:1000]
+	#pfp(bred, "Warning, using a limit of 1000 globals", rst)
 	globsets = split_set(globs)
 def load_imagenames_recursively(path):
 	files = []
-	for path, dirs, names in os.walk(os.path.join(path, "images")):
+	prog = re.compile('\.(jpg|gif|tif|tiff|png|bmp)$', re.IGNORECASE)
+	for path, dirs, names in os.walk(path):
 		#print "Path: ", path
 		#print "Names: ", names
 		for fn in names:
-			if not fn.startswith("."):
+			if not fn.startswith(".") and prog.search(fn):
 				files.append(os.path.join(path, fn))
 	#print "File list: ", files
 	return files
@@ -99,32 +111,33 @@ def load_labeled():
 	global label_ints
 	global lbl_cnt
 	lbld = dict()
-	for path, dirs, names in os.walk(config.dir_labeled_base):
-		for labeldir in dirs:
-			label_names.append(labeldir)
-			files = load_imagenames_recursively(os.path.join(path, labeldir))
-			#print "Files returned: ", files
-			#exit(0)
+	base = config.dir_labeled_base
+	label_names = [ fn for fn in os.listdir(base) if isdir(os.path.join(base, fn)) ]
+	for label in label_names:
+		#pf("Processing label folder:", label)
+		files = load_imagenames_recursively(os.path.join(base, label))
+		#print "  Files returned: ", files
+		#exit(0)
+		if not args.noshuffle:
 			random.shuffle(files)
-			sets = split_set(files)
-			for ttv in sets.keys(): # train, test, val
-				if not lbld.has_key(ttv):
-					lbld[ttv]=[]
-				for img in sets[ttv]: # filenames
-					#print("Appending img,labeldir:", img, labeldir)
-					lbld[ttv].append([img, labeldir])
-			dirs.remove(labeldir)
-	#print "Label names:", label_names
+		sets = split_set(files)
+		for ttv in sets.keys(): # train, test, val
+			if not lbld.has_key(ttv):
+				lbld[ttv]=[]
+			for img in sets[ttv]: # filenames
+				lbld[ttv].append([img, label])
 
 	lbl_cnt = len(label_names)+1 # add one for global label as 0
 	pf("Total labels, including global:", lbl_cnt)
-	i=1 # Start at 1 to skip global image label at 0
 	label_names.sort()
-	pf("Number of non-global labels: ", len(label_names))
+	#pf("Number of non-global labels: ", len(label_names))
+	i=1 # Start at 1 to skip global image label at 0
 	for lbl in label_names:
 		label_onehots[lbl] = int_to_onehot(i)
 		label_ints[lbl] = i
 		i += 1
+	label_ints['global'] = 0
+	label_onehots['global'] = int_to_onehot(0)
 	#pf("Labeled sets:", lbld);
 	#pf("Labeled set:", lbld['train']);
 	#print "Label onehots:", label_onehots
@@ -162,6 +175,8 @@ def load_args():
 	parser.add_argument('-f', '--find', default=None, help='Find images matching label')
 	parser.add_argument('-v', '--verbose', default=0, help='Set verbosity 0, 1, etc.')
 	parser.add_argument('-c', '--showcaching', action='store_true', help='Show image caching process')
+	parser.add_argument('-C', '--showcachetrue', action='store_true', help='Show image caching successful hits')
+	parser.add_argument('-S', '--noshuffle', action='store_true', help='Disable random file shuffling')
 	args=parser.parse_args()
 class SaveWeights(keras.callbacks.Callback):
 	def on_epoch_end(self, epoch, logs={}):
@@ -206,32 +221,30 @@ def show_a_prediction(label="", model=None, imgsetname=None):
 def label_to_onehot(lbl):
 	return label_onehots[lbl]
 def prep_img(fn):
-	img = get_cached(fn)
-	#if img:
-		#if args.showcaching:
-			#pfp(bgre, "Using cached image: ", fn, rst)
-	if img == None:
+	try:
+		img = get_cached(fn)
 		#pf("Preparing image:", fn)
+	except: # image not found in cache
 		try:
 			img = load_img(fn) #, grayscale=grayscale)
 		except:
 			pf("ERROR: Error loading image:", fn, "\n", sys.exc_info()[0])
-			return None
-		if img == None:
-			pfp(bred, "Error loading image: ", fn, rst)
-			return None
-		else:
-			if args.showcaching:
-				pfp(bcya, "Caching image: ", fn, rst)
-			#img = img.resize((indim,indim), resample=PIL.Image.LANCZOS)
-			img.thumbnail((indim,indim), PIL.Image.ANTIALIAS)
-			bg = Image.new('RGB', (indim,indim), (0, 0, 0))
-			bg.paste(
-			    	img,
-					(int((indim - img.size[0]) / 2), int((indim - img.size[1]) / 2))
-				)
-			img=bg
+			raise
+		if args.showcaching:
+			pfp(bcya, "Caching image: ", fn, rst)
+		#img = img.resize((indim,indim), resample=PIL.Image.LANCZOS)
+		img.thumbnail((indim,indim), PIL.Image.ANTIALIAS)
+		bg = Image.new('RGB', (indim,indim), (0, 0, 0))
+		bg.paste(
+				img,
+				(int((indim - img.size[0]) / 2), int((indim - img.size[1]) / 2))
+			)
+		img=bg
+		try:
 			store_cache(fn, img)
+		except:
+			pf("Error storing image to", fn)
+			exit(0)
 
 	img = img_to_array(img)  # Numpy array with shape (?, width, height)
 	img = img/255.0
@@ -241,16 +254,19 @@ def prep_img(fn):
 def get_cached(fn):
 	cdir, d2, hfull = fn2hashpath(fn)
 	hfull = hfull + ".jpg"
-	if not os.path.exists(hfull): return None
+	if not os.path.exists(hfull):
+		raise IOError
 	#pf("Loading cached image:", hfull)
 	try:
 		f = Image.open(hfull)
 	except IOError as e:
 		pf("I/O error({0}): {1}".format(e.errno, e.strerror))
-		return None
+		raise
 	except: #handle other exceptions such as attribute errors
 		pf("Unexpected error:", sys.exc_info()[0])
-		return None
+		raise
+	if args.showcachetrue:
+		pf(bcya, "Using cached image: ", fn, "as", hfull, rst)
 	return f
 
 def store_cache(fn, img):
@@ -263,10 +279,10 @@ def store_cache(fn, img):
 		img.save(hfull, "JPEG", quality=87)
 	except IOError as e:
 		pf("I/O error({0}): {1}".format(e.errno, e.strerror))
-		return None
+		raise
 	except: #handle other exceptions such as attribute errors
 		pf("Unexpected error:", sys.exc_info()[0])
-		return None
+		raise
 #output:
 # caching cache/83/83a1ff93f5f6ea500bfd5aec008507...
 # Unexpected error: <type 'exceptions.ValueError'>
@@ -282,25 +298,42 @@ def fn2hashpath(fn):
 	return cac, d2, hpath
 
 def source_imgs(imgsetname): # imgsetname='train','val','test'
+	train_datagen = ImageDataGenerator(
+			shear_range=0.2,
+			zoom_range=0.2,
+			horizontal_flip=True,
+		)
 	while True:
 		#pf("source_imgs(", imgsetname, ") called")
+		
+		# Labeled set
 		if (random.random() < labeled_frac):
 			# choose from labeled set
 			fn_n_label_i = random.randint(0, len(lbld[imgsetname])-1)
 			fn_n_label = lbld[imgsetname][fn_n_label_i]
-			ximg = prep_img(fn_n_label[0])
-			if ximg == None:
+			fn = fn_n_label[0]
+			try:
+				ximg = prep_img(fn)
+			except:
+				pf(bred, "Image load failed.  Trying again:", fn, rst)
 				lbld.pop(fn_n_label_i)
 				continue
+			#pf(bgre, "Image load success:", fn, rst)
+			#pf("ximg.shape", ximg.shape)
+			#view_img("Orig", ximg[0])
+			#view_img("Xform", ximg[0])
+			ximg[0] = train_datagen.random_transform(ximg[0])
 			lbl = label_to_onehot(fn_n_label[1])
 			#pf("LBLD:", lbl, fn_n_label[0])
+		# Global set
 		else:
 			#print globsets;
 			fni = random.randint(0, len(globsets[imgsetname])-1)
 			fn = globsets[imgsetname][fni]
 			#pf("GLOB fn", fn)
-			ximg = prep_img(fn)
-			if ximg == None:
+			try:
+				ximg = prep_img(fn)
+			except:
 				globsets[imgsetname].pop(fni)
 				continue
 			lbl = int_to_onehot(0)   # global is 0!
@@ -308,6 +341,8 @@ def source_imgs(imgsetname): # imgsetname='train','val','test'
 		#pf("Yielding ximg shape = ", ximg.shape, sep='')
 		#pf("Y shape", lbl.shape)
 		#pf("Yielding: y=", lbl, " data ", ximg, sep='')
+		#if "guitar" in fn:
+		#pf("Categorizing", lbl, " <- ", fn)
 		yield ximg, lbl
 
 def make_model():
@@ -323,38 +358,39 @@ def make_model():
 #	x = conv2d_leaky(x, filters=64, filtdim=2, ss=(1,1), border='valid', name='first')
 #	x = conv2d_leaky(x, filters=64, filtdim=2, ss=(2,2), border='valid', name='first')
 	# 64
-	filt=64
+	filt=100
 	x = Convolution2D(filt, 2, 2, border_mode='valid', activation='relu', W_constraint=maxnorm(3))(x)
 	x = Dropout(0.2)(x)
 	x = Convolution2D(filt, 2, 2, border_mode='valid', activation='relu', W_constraint=maxnorm(3))(x)
-	x = MaxPooling2D((2, 2))(x)
+	x = Convolution2D(filt, 2, 2, border_mode='valid', activation='relu', W_constraint=maxnorm(3), subsample=(2,2))(x)
+	#x = MaxPooling2D((2, 2))(x)
 	# 32 or something
 	x = Convolution2D(filt*2, 2, 2, border_mode='valid', activation='relu', W_constraint=maxnorm(3))(x)
-	x = Convolution2D(filt*2, 2, 2, border_mode='valid', activation='relu', W_constraint=maxnorm(3))(x)
-	x = MaxPooling2D((2, 2))(x)
+	x = Convolution2D(filt*2, 2, 2, border_mode='valid', activation='relu', W_constraint=maxnorm(3), subsample=(2,2))(x)
+	#x = MaxPooling2D((2, 2))(x)
 	# 16 or something
 	x = Convolution2D(filt*4, 2, 2, border_mode='valid', activation='relu', W_constraint=maxnorm(3))(x)
-	x = Convolution2D(filt*4, 2, 2, border_mode='valid', activation='relu', W_constraint=maxnorm(3))(x)
-	x = MaxPooling2D((2, 2))(x)
+	x = Convolution2D(filt*4, 2, 2, border_mode='valid', activation='relu', W_constraint=maxnorm(3), subsample=(2,2))(x)
+	#x = MaxPooling2D((2, 2))(x)
 	# 8 or something
 	x = Convolution2D(filt*8, 2, 2, border_mode='valid', activation='relu', W_constraint=maxnorm(3))(x)
-	x = Convolution2D(filt*8, 2, 2, border_mode='valid', activation='relu', W_constraint=maxnorm(3))(x)
-	x = MaxPooling2D((2, 2))(x)
+	x = Convolution2D(filt*8, 2, 2, border_mode='valid', activation='relu', W_constraint=maxnorm(3), subsample=(2,2))(x)
+	#x = MaxPooling2D((2, 2))(x)
 	# 4 or something
 	x = Flatten()(x)
-	x = Dense(512, activation='relu', W_constraint=maxnorm(3))(x)
-	x = Dropout(0.1)(x)
+	x = Dense(256, activation='relu', W_constraint=maxnorm(3))(x)
+	x = Dropout(0.05)(x)
 	num_classes=lbl_cnt
 	x = Dense(num_classes, activation='softmax')(x)
 	model = Model(input=inputs, output=x)
 	# Compile model
 	epochs = train_epochs
 	lrate = 0.001
-	decay = lrate/epochs
-	#sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
-	sgd = SGD(lr=lrate, momentum=0.9, nesterov=False)
+	decay = 0.01/epochs
+	sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
+	#sgd = SGD(lr=lrate, momentum=0.9, nesterov=False)
 	model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-	print(model.summary())
+	pf(model.summary())
 	if load_weights and isfile(weight_store):
 		pf("Loading weights")
 		model.load_weights(weight_store)
@@ -407,26 +443,87 @@ def find_imgs(lbl):
 		##fn = fn[0]
 		#pf("file:", fn)
 		count = count+1
-		img = prep_img(fn)
-		if img == None:
+		try:
+			img = prep_img(fn)
+		except:
 			allglobs.pop(fni)
 			fni = fni-1
 			pf("ERROR: Removed global fn:", fn)
 			continue
 		y = model.predict(img, batch_size=1, verbose=0)
-		matches.append([fn, y[0][lbli]])
-		if y[0][lbli] >= args.threshold:
-			pf("Predict:", fn, " -> y:", y)
-			pf("  y[0][", lbli, "] = ", y[0][lbli])
-			pf("  fn:", fn)
+		matches.append([fn, y[0]])
+		#pf(y[0])
+		if y[0][lbli] >= 0: #args.threshold:
+			#pf("Predict:", fn, " -> y:", y)
+			#pf("y:", y)
+			#pfp("  y[0][", lbl, "=", lbli, "] = ", y[0][lbli])
+			#pf("  fn:", fn)
+			pass
 		if (count%100) == 0:
 			pf("Searched", count, "of", globcount)
-	matches = sorted(matches, key=sort_y, reverse=True)
+	#matches = sorted(matches, key=sort_y, reverse=True)
+	#pf(bred, "Matches[0]: ", matches[0], rst)
+	#pf(bmag, "Matches[0][1]: ", matches[0][1], rst)
+	#pf(bcya, "lbl:", lbl, lbli, rst)
+	matches = sorted(matches, reverse=True, key=lambda s: s[1][lbli])
+	pf("Labels:")
+	for s in label_ints.keys():
+		pf(" ", label_ints[s], s)
 	pf("Results:")
 	for m in matches:
-		pf(m[1], m[0])
+		pf(" ", m[1], m[0])
+		pf(m[1][lbli], m[0])
 def sort_y(a):
 	return a[1]
+
+def view_img(label, img, show=False, transpose=True):
+	global axs
+	global fig_view
+	global whichsubplot
+	pf("View img", end='')
+	#img = mpimg.imread('stinkbug.png')
+	plotrows = 5
+	plotcols = 6
+	#pf(img)
+	whichsubplot += 1
+	if (whichsubplot >= plotrows*plotcols):
+		whichsubplot = 0
+	#pf(fig_view)
+	if fig_view is None:
+		fig_view,axs = plt.subplots(plotrows,plotcols,figsize=(12,9))
+		plt.ion()
+		plt.pause(0.05) # Calls matplotlib's event loop
+		fig_view.subplots_adjust(hspace=0)
+		fig_view.subplots_adjust(wspace=.3)
+	#pf(fig_view)
+	plt.figure(fig_view_num)
+	#pf("  Plotting whichsubplot:", whichsubplot)
+	yax = int(whichsubplot/plotcols)
+	xax = int(whichsubplot % plotcols)
+	#pf("  Current axes (y,x):", yax, xax)
+	#pf("  Deleting...")
+	fig_view.delaxes(axs[yax][xax]) 
+	#pf("  Making new subplot...")
+	newaxs = plt.subplot(plotrows, plotcols, whichsubplot+1)
+	axs[yax][xax] = newaxs
+	plt.title(label, fontsize=10)
+	plt.axis('off')
+	img = img.transpose(1,2,0)
+	if len(img.shape) == 2:
+		axs[yax][xax].imshow(img, cmap="gray", interpolation='nearest') # interpolation='None'
+	else:
+		if grayscale:
+			axs[yax][xax].imshow(img[:,:,0], cmap="gray", interpolation='nearest') # interpolation='None'
+		else:
+			axs[yax][xax].imshow(img, interpolation='nearest') # interpolation='None'
+	plt.pause(0.05) # Calls matplotlib's event loop
+	#plt.colorbar()
+	#plt.axes.get_xaxis().set_visible(False)
+	#plt.axes.get_yaxis().set_visible(False)
+	#if show:
+		#plt.show()
+		#plt.pause(0.05) # Calls matplotlib's event loop
+	pf("/view img")
 
 init()
 model=make_model()
